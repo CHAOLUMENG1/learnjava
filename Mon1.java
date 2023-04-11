137277b119659eb096a6bcdd810a412ca00a9966
CHAOLUMENG1 <129593598+CHAOLUMENG1@users.noreply.github.com>
2023-04-11 19:29:38 +0900

    Add files via upload

---
+++ Mon1.java
@@ -0,0 +1,27 @@
+package jp.dcnet.it.c03;
+
+public class Mon1 {
+	/**
+	 * 成績ランキング判断を作成する「90以上︓A, 80-90︓B, 70-80︓C, 60-70︓D, 60以下︓E」
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		int a = 56;
+		System.out.println("成績ランキング判断。入力値=" + a);
+
+		if (a >= 90) {
+			System.out.println("A");
+		} else if (a >= 80) {
+			System.out.println("B");
+		} else if (a >= 70) {
+			System.out.println("C");
+		} else if (a >= 60) {
+			System.out.println("D");
+		} else {
+			System.out.println("E");
+		}
+
+	}
+
+}
---
+++ Mon2.java
@@ -0,0 +1,32 @@
+package jp.dcnet.it.c03;
+
+public class Mon2 {
+	/**
+	 * ⽇本のコインを１円、５円、１０円、１００円、５００円があります。３３４０
+	円のコイン数をもとめください。
+	 * @param args
+	 */
+
+	public static int[] YEN_TYPES = { 500, 100, 50, 10, 5, 1 };
+
+	public static int[] getYenCount(int yen, int[] yentype) {
+		int[] count = new int[yentype.length];
+
+		for (int i = 0; i < yentype.length; i++) {
+			while (yen >= yentype[i]) {
+				yen -= yentype[i];
+				count[i]++;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		int yen = 3340;
+		int[] count = getYenCount(yen, YEN_TYPES);
+		for (int i = 0; i < YEN_TYPES.length; i++) {
+
+			System.out.println(YEN_TYPES[i] + "円:" + count[i] + "枚");
+		}
+	}
+}
