0cc738966eb23e2834a18e69efe47e4e84517d08
CHAOLUMENG1 <129593598+CHAOLUMENG1@users.noreply.github.com>
2023-04-11 19:28:31 +0900

    Add files via upload

---
+++ Hensuu_1.java
@@ -0,0 +1,18 @@
+package jp.dcnet.it.c01;
+
+public class Hensuu_1 {
+	/**
+	 * 変数nの値は︖
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+		int i = 10;
+		int n = i % 5;
+
+		System.out.print(n);
+
+	}
+
+}
