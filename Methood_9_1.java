846421436e01bc91269e50633cb0ae55de55766f
CHAOLUMENG1 <129593598+CHAOLUMENG1@users.noreply.github.com>
2023-04-12 18:41:01 +0900

    Add files via upload

---
+++ Methood_9_1.java
@@ -0,0 +1,62 @@
+package jp.dcnet.it.c06;
+
+public class Methood_9_1 {
+
+	public static void main(String[] args) {
+
+		int[] left = new int[] { 1, 2, 3, 4 };
+		int[] right = new int[] { 5, 6, 7, 8 };
+		int[] result = megerArray(left, right);
+
+		printArray(result);
+
+	}
+
+	/**
+	 * 引数left と 引数right をマージする
+	 * @param left
+	 * @param right
+	 * @return
+	 */
+	public static int[] megerArray(int[] left, int[] right) {
+
+		int[] result = new int[left.length + right.length];
+
+		for (int i = 0; i < left.length; i++) {
+
+			result[i] = left[i];
+
+		}
+
+		for (int j = 0; j < right.length; j++) {
+
+			result[j + left.length] = right[j];
+
+		}
+
+		return result;
+
+	}
+
+	/**
+	 * Print
+	 * @param objs
+	 */
+	public static void printArray(int[] objs) {
+		if (objs == null) {
+			System.out.println("null");
+			return;
+		}
+		String prefix = "";
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		for (Object obj : objs) {
+			sb.append(prefix);
+			sb.append(String.valueOf(obj));
+			prefix = ", ";
+		}
+		sb.append("]");
+		System.out.println(sb.toString());
+	}
+
+}
